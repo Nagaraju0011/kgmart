be0da019f8ee3514dfddb83e0b7cc4a8368e3779
Nagaraju SMC2 <nagaraju.komma@smc2.com>
2022-04-02 16:20:34 +0530

    code push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header'
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
